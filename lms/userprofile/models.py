aa2275c17f5a6f1c845d2e98c670b71c04788e7f
amrabouelenin <amr@new-track.net>
2021-02-27 15:05:33 +0000

    add phone number, year and verified fields

--- lms/userprofile/models.py
+++ lms/userprofile/models.py
@@ -4,12 +4,25 @@ from django.contrib.auth.models import AbstractUser
 from itp.models import JobRole
 from emp_entity.models import EmployeeEntity
 from django_countries.fields import CountryField
+
+from phonenumber_field.modelfields import PhoneNumberField
+import datetime
+
+# Inherit default user model
 class User(AbstractUser):
     username = models.CharField(unique=True,max_length=150, verbose_name = 'Login Id')
 
+
 # Custom user profile to extend default user model
 class UserProfile(models.Model):
-
+  
+    # Lis of years - choices +5, -5 current year
+    Years = []
+    start_year = datetime.datetime.now().year - 5
+    for i in range(10):
+        start_year += 1
+        Years.append((start_year,start_year))
+   
     # Link to default user model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
@@ -21,5 +34,12 @@ class UserProfile(models.Model):
     
     # Nationality shall read from country field
     nationality = CountryField(default= 1, null = False, blank_label='(select country)')
+    
+    # Mobile number
+    phone_number = PhoneNumberField(default='+48691971923', verbose_name= 'Phone Number')
+    
+    # year the user has been added
+    year = models.IntegerField(choices=Years, default=start_year-5)
 
-
+    # Verified
+    verified = models.IntegerField(choices = [(0, 'Not Verified'),(1, 'Verified')], default =0, help_text = 'please check this if you have verfieid the user')
