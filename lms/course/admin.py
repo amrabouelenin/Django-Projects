b28b95913221db1c5a325d5059f782160a0e2ccb
amrabouelenin <amr@new-track.net>
2021-02-28 13:02:48 +0000

    delete accounts, modify urls, add rich text, and create course model

--- lms/course/admin.py
+++ lms/course/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from course.models import Course, Category, TargetAudience
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Category)
+admin.site.register(TargetAudience)
--- lms/course/models.py
+++ lms/course/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from djrichtextfield.models import RichTextField
 
-# Create your models here.
+
+# category of the course
+class Category(models.Model):
+    # Name of category
+    name = models.CharField(max_length=255, unique= True, help_text = "Category")
+    def __str__(self):
+        return self.name
+
+# Target Audience of the course
+class TargetAudience(models.Model):
+
+    # Name of category
+    name = models.CharField(max_length=255, unique= True, help_text = "Target Audience")
+
+    def __str__(self):
+        return self.name
+
+
+
+
+# Main course model
+class Course(models.Model):
+    course_name = models.CharField(unique=True, max_length=255, verbose_name = "Course Name")
+    course_overview = RichTextField(help_text = "Write an overview about the course", verbose_name = 'Overview')
+    course_objective = RichTextField(help_text = "Write the objective of the course", verbose_name = 'Objective')
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.DO_NOTHING)
+    target_audience = models.ForeignKey(TargetAudience, null=True, blank=True, on_delete=models.DO_NOTHING)
+    prerequisites = RichTextField(help_text = "Write the Prerequisites of the course", verbose_name = 'Prerequisites')
+    course_img = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=100, help_text = 'make it clean and neat. ;) ')
+    prerequisites_courses = models.ForeignKey('self', null=True, blank=True, on_delete=models.DO_NOTHING)
+
+
+
+
+
