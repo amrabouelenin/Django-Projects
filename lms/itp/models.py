b7908114ecaaf6551ecc45060335cdd15e05e436
amrabouelenin <amr@new-track.net>
2021-03-01 08:14:21 +0000

    defining ITP required model

--- lms/itp/models.py
+++ lms/itp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+#from userprofile.models import User
+import datetime
+
 
 # ITP required models
 
@@ -31,3 +34,40 @@ class JobRole(models.Model):
     job_role = models.CharField(max_length=256, verbose_name = 'Standard ITP Role')
     # reference to unit name
     unit_name = models.ForeignKey(Unit, null = False, on_delete = models.DO_NOTHING, related_name = 'jobrole_unit')
+
+
+class ITPCourse(models.Model):
+
+    # Course Name
+    course_name = models.CharField(unique=True, max_length=255, verbose_name = "Course Name")
+   
+    # Priority
+    ESSENTIAL_REQUIRED = 'essential_required'
+    REQUIRED_TRAINING = 'required_training'
+    FUTURE_REQUIRED = 'future_reuired'
+    FUTURE_DEVELOPMENT = 'future_development'
+    PRIORITY_LIST = [
+        (ESSENTIAL_REQUIRED, 'Essential Required'),
+        (REQUIRED_TRAINING, 'Required Training'),
+        (FUTURE_REQUIRED, 'Future Required'),
+        (FUTURE_DEVELOPMENT, 'Future Development'),
+    ]
+    # priority
+    priority= models.CharField(max_length=120,choices=PRIORITY_LIST, default=ESSENTIAL_REQUIRED)
+
+ 
+    # Certificate Type
+    PROFESSIONAL_CERTIFIED = 'professional_certified'
+    PARTICIPANT_CERTIFIED = 'participant_certified'
+    CERTFIEICATE_LIST = [
+        (PROFESSIONAL_CERTIFIED, 'Professional Certified'),
+        (PARTICIPANT_CERTIFIED, 'Participant Certfied'),
+    ]
+    # certificate type
+    certificate_type= models.CharField(max_length=120,choices=CERTFIEICATE_LIST, default=PROFESSIONAL_CERTIFIED)
+    remarks = models.TextField(blank = True)
+
+    # link to itp
+
+
+ 
